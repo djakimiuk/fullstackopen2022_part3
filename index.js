a65186b94e0f523ddf67288961728f38ff7f7c65
djakimiuk <djakimiuk@outlook.com>
2023-07-18 00:07:10 +0200

    ex. 3.22: Lint configuration solved

--- index.js
+++ index.js
@@ -1,100 +1,100 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const Contact = require("./models/contact");
-const cors = require("cors");
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static("build"));
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const Contact = require('./models/contact')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
 const errorMiddleware = (error, req, res, next) => {
-  console.error(error.message);
+    console.error(error.message)
 
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
-  }
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
+    }
 
-  next(error);
-};
+    next(error)
+}
 
-morgan.token("body", (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-app.get("/api/persons", (req, res) => {
-  Contact.find({}).then((contacts) => {
-    res.json(contacts);
-  });
-});
-
-app.get("/api/persons/:id", (req, res) => {
-  Contact.findById(req.params.id).then((contact) => {
-    res.json(contact);
-  });
-});
-
-app.get("/info", (req, res) => {
-  Contact.estimatedDocumentCount()
-    .then((count) => {
-      res.send(`<p>Phonebook has info for ${count} people</p>
-    <p>${new Date()}</p>`);
+app.get('/api/persons', (req, res) => {
+    Contact.find({}).then((contacts) => {
+        res.json(contacts)
     })
-    .catch((err) => {
-      console.log(`There was an error: ${err}`);
-    });
-});
+})
 
-app.delete("/api/persons/:id", (req, res) => {
-  Contact.findByIdAndRemove(req.params.id).then((contact) => {
-    res.status(204).end();
-  });
-});
-
-app.post("/api/persons", (req, res, next) => {
-  const body = req.body;
-  if (!body || !body.name || !body.number) {
-    return res.status(400).json({ error: "name and number must be filled in" });
-  }
-
-  const person = new Contact({
-    name: body.name,
-    number: body.number,
-  });
-
-  person
-    .save()
-    .then((savedPerson) => {
-      res.json(savedPerson);
+app.get('/api/persons/:id', (req, res) => {
+    Contact.findById(req.params.id).then((contact) => {
+        res.json(contact)
     })
-    .catch((error) => next(error));
-});
+})
 
-app.put("/api/persons/:id", (req, res, next) => {
-  const body = req.body;
-  const updatedPerson = {
-    name: body.name,
-    number: body.number,
-  };
-  Contact.findByIdAndUpdate(req.params.id, updatedPerson, {
-    new: true,
-    runValidators: true,
-    context: "query",
-  })
-    .then((contact) => {
-      res.json(contact);
+app.get('/info', (req, res) => {
+    Contact.estimatedDocumentCount()
+        .then((count) => {
+            res.send(`<p>Phonebook has info for ${count} people</p>
+    <p>${new Date()}</p>`)
+        })
+        .catch((err) => {
+            console.log(`There was an error: ${err}`)
+        })
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    Contact.findByIdAndRemove(req.params.id).then((contact) => {
+        res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+})
 
-app.use(errorMiddleware);
+app.post('/api/persons', (req, res, next) => {
+    const body = req.body
+    if (!body || !body.name || !body.number) {
+        return res.status(400).json({ error: 'name and number must be filled in' })
+    }
 
-const PORT = process.env.PORT || 3001;
+    const person = new Contact({
+        name: body.name,
+        number: body.number,
+    })
+
+    person
+        .save()
+        .then((savedPerson) => {
+            res.json(savedPerson)
+        })
+        .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    const updatedPerson = {
+        name: body.name,
+        number: body.number,
+    }
+    Contact.findByIdAndUpdate(req.params.id, updatedPerson, {
+        new: true,
+        runValidators: true,
+        context: 'query',
+    })
+        .then((contact) => {
+            res.json(contact)
+        })
+        .catch((error) => next(error))
+})
+
+app.use(errorMiddleware)
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
--- models/contact.js
+++ models/contact.js
@@ -1,43 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set(`strictQuery`, false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log(`connecting to ${url}`);
+console.log(`connecting to ${url}`)
 
 mongoose
-  .connect(url)
-  .then((result) => {
-    console.log(`connected to MongoDB`);
-  })
-  .catch((error) => {
-    console.log(`error connecting to MongoDB: ${error.message}`);
-  });
+    .connect(url)
+    .then((result) => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log(`error connecting to MongoDB: ${error.message}`)
+    })
 
 const contactSchema = new mongoose.Schema({
-  name: { type: String, minLength: 3, required: true },
-  number: {
-    type: String,
-    validate: {
-      validator: function (v) {
-        return /^\d{2,3}-\d{6,}$/.test(v);
-      },
-      message: (props) =>
-        `${props.value} is not a valid phone number! eg. 09-1234556 and 040-22334455 are valid phone numbers`,
+    name: { type: String, minLength: 3, required: true },
+    number: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d{6,}$/.test(v)
+            },
+            message: (props) =>
+                `${props.value} is not a valid phone number! eg. 09-1234556 and 040-22334455 are valid phone numbers`,
+        },
+        required: true,
     },
-    required: true,
-  },
-});
+})
 
-const Contact = mongoose.model("Contact", contactSchema);
+const Contact = mongoose.model('Contact', contactSchema)
 
-contactSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+contactSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+})
 
-module.exports = mongoose.model("Contact", contactSchema);
+module.exports = mongoose.model('Contact', contactSchema)
